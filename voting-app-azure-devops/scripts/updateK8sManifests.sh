76929e092d4b3a0ddd40d890bb32fcb865aff7c2
dee <techdeepa20@gmail.com>
2026-01-13 13:16:17 +0530

    Added the voting-app-azure-devops folder

---
+++ voting-app-azure-devops/scripts/updateK8sManifests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sed -i 's/\r$//' "$0"
+# Set the repository URL
+REPO_URL="https://PAT@dev.azure.com/techflavour/voting-app/_git/voting-app"
+repo_url="$(echo "$repo_url" | tr -d '\r')"
+# Clone the git repository into the /tmp directory
+git clone "$REPO_URL" /tmp/temp_repo
+# Navigate into the cloned repository directory
+cd /tmp/temp_repo
+# Make changes to the Kubernetes manifest file(s)
+# For example, let's say you want to change the image tag in a deployment.yaml file
+sed -i "s|image:.*|image: techflavour.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
+# Add the modified files
+git add .
+# Commit the changes
+git commit -m "Update Kubernetes manifest"
+# Push the changes back to the repository
+git push
+# Cleanup: remove the temporary directory
+rm -rf /tmp/temp_repo
